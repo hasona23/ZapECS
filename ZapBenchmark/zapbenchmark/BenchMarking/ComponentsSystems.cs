c216418e1b8469f77d9f2a5826456e1bc93dae3b
hasona23 <hassan20302008@gmail.com>
2024-07-11 03:55:39 +0300

    Change folder name to benchmark

---
+++ ZapBenchmark/zapbenchmark/BenchMarking/ComponentsSystems.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Zap_ecs.Core;
+using Zap_ecs;
+namespace banchmark;
+    public class Sprite : IComponent
+    {
+        public Texture2D texture;
+        public Sprite(Texture2D t) { texture = t; }
+    }
+    public class  Transform:IComponent
+    {
+        public Vector2 pos;
+        public float scale;
+
+        public Transform(Vector2 pos, float scale = 2f)
+        {
+            this.pos = pos;
+            this.scale = scale;
+        }
+    }
+    public class  Velocity:IComponent
+    {
+        public Vector2 vel;
+        public float speed;
+
+        public Velocity(Vector2 vel, float speed)
+        {
+            this.vel = vel;
+            this.speed = speed;
+        }
+    }
+
+
+    public class drawSystem : GameSystem
+    {
+        public drawSystem():base()
+        {
+            SetRequirments(new Type[2] {typeof(Sprite),typeof(Transform)});
+        }
+        public override void Update(GameTime gt, SpriteBatch sb = null)
+        {
+            if (sb != null)
+            {
+                foreach (var entity in this.entities)
+                {
+                    var sprite = entity.GetComponent<Sprite>();
+                    var transform = entity.GetComponent<Transform>();
+                    sb.Draw(sprite.texture, new Rectangle(new Point((int)transform.pos.X, (int)transform.pos.Y), new Point((int)(transform.scale * sprite.texture.Width), (int)(transform.scale * sprite.texture.Width))), Color.White);
+                }
+            }
+        }
+    }
+    public class MoveSystem : GameSystem
+    {
+        public MoveSystem():base()
+        {
+            SetRequirments(new Type[2] { typeof(Transform), typeof(Velocity) });
+        }
+
+        public override void Update(GameTime gt, SpriteBatch sb = null)
+        {
+            foreach (var entity in this.entities)
+            {
+                var velocity = entity.GetComponent<Velocity>();
+                var transform = entity.GetComponent<Transform>();
+                if (velocity.vel.Length()>0) {
+                    velocity.vel.Normalize();
+                }
+                var changeX = velocity.vel.X * velocity.speed;
+                var changeY = velocity.vel.Y * velocity.speed;
+                if (transform.pos.X + changeX >= Globals.WindowSize.X || transform.pos.X+changeX <= 0)  
+                {
+                    velocity.vel.X *= -1;
+                }
+            transform.pos.X += changeX;
+            if (transform.pos.Y + changeY >= Globals.WindowSize.Y || transform.pos.Y + changeY <= 0)
+            {
+                velocity.vel.Y *= -1;
+            }
+            transform.pos.Y += changeY;
+
+            }
+
+        }
+
+        
+        
+    }
+
